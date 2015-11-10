6427f5ae6663dc1a370aa42a46a0f8fc19107020
Lundis <lundis.bah@hotmail.com>
2015-11-10 17:47:15 +0200

    Use windows.InitWindowing().

--- demos/cube_alpha1/try2.go
+++ demos/cube_alpha1/try2.go
@@ -7,14 +7,14 @@ import (
 	"image"
 	"image/draw"
 	_ "image/png"
-	"log"
 	"os"
 	"runtime"
 	"strings"
 
-	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/go-gl/glfw/v3.1/glfw"
 	"github.com/go-gl/mathgl/mgl32"
+	. "github.com/krapulacoders/krapulaengine2/windows"
 )
 
 const windowWidth = 800
@@ -26,16 +26,9 @@ func init() {
 }
 
 func main() {
-	if err := glfw.Init(); err != nil {
-		log.Fatalln("failed to initialize glfw:", err)
-	}
+	InitWindowing()
 	defer glfw.Terminate()
 
-	glfw.WindowHint(glfw.Resizable, glfw.False)
-	glfw.WindowHint(glfw.ContextVersionMajor, 4)
-	glfw.WindowHint(glfw.ContextVersionMinor, 1)
-	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
 	window, err := glfw.CreateWindow(windowWidth, windowHeight, "Cube", nil, nil)
 	if err != nil {
 		panic(err)
